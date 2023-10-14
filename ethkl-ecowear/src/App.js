aec7d2dfb206ef086455b0e46e0c255974de992a
xInr0u <xinrou1961@gmail.com>
2023-10-14 13:35:13 +0800

    HomeView
    a brief homeview to start off

---
+++ ethkl-ecowear/src/App.js
@@ -0,0 +1,30 @@
+import { ethers } from "ethers"
+import './App.css';
+//import { abi } from "./abi"
+
+function App() {
+
+  return (
+    <div>
+      <div className="container mx-auto max-w-6xl p-8 2xl:px-0">
+        <header className="App-header">
+          <div className="hero content">
+            <h1 className="mb-5 text-5xl font-display">
+              Welcome to EcoWear.
+            </h1>
+            
+            <p className="mb-5">
+              building eco-friendly and socially responsible fashion ecosystem
+            </p>
+            <div className="card">
+
+            </div>
+          </div>
+        
+        </header>
+     </div>
+    </div>
+  );
+}
+
+export default App;
