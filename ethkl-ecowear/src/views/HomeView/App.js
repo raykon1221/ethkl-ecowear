c09dfec9b0f4032dedb071bc837e477fb66d198e
xInr0u <xinrou1961@gmail.com>
2023-10-14 17:30:24 +0800

    edited some changes on the home page

--- ethkl-ecowear/src/views/HomeView/App.js
+++ ethkl-ecowear/src/views/HomeView/App.js
@@ -1,29 +1,36 @@
+import { FC } from "react";
 import { ethers } from "ethers"
 import './App.css';
+import * as React from 'react';
 //import { abi } from "./abi"
 
 function App() {
 
   return (
     <div>
-      <div className="container mx-auto max-w-6xl p-8 2xl:px-0">
+      <div className="container mx-auto max-w-5xl p-4 2xl:px-0">
         <header className="App-header">
-          <div className="flex-flex hero content">
-            <h1 className="mb-5 text-5xl font-display">
+          <div style={{ display: "flex"}} className=" hero content">
+            <h1 className="mb-2 ml-10 text-3xl font-display">
               Welcome to EcoWear.
             </h1>
-            <button> Click Me</button>
-            <p className="mb-5">
-              building eco-friendly and socially responsible fashion ecosystem
-            </p>
-            <div className="card">
-
+        
+          
+            <div style={{ margin: "50px 0", flexDirection: "row", alignItems: "flex-start", justifyContent: "flex-start" }}>
+              <button className="custom-button">Buy</button>
+              <button className="custom-button">Sell</button>
+              <button className="custom-button">Manufacturer</button>
             </div>
           </div>
-        
+          <div>
+            <p className="mb-1">
+              building eco-friendly and socially responsible fashion ecosystem
+            </p>
+            <div className="card"></div>
+          </div>
         </header>
-     </div>
-    </div>
+      </div>
+   </div>
   );
 }
 
